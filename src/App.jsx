20f0821a8b66dc577392a9f815beba077bf8241c
alid994 <aljo_dozer@hotmail.com>
2023-12-25 12:44:53 +0100

    created register component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './components/login/login';
+import Register from './components/register/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,50 +1,50 @@
 import Footter from './footter';
 import './login.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
-function Login () {
+export default function Login() {
 
   const baseUrl = 'http://localhost:8080';
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    function handleUsernameChange(e) {
-      setUsername(e.target.value);
-    }
+  function handleUsernameChange(e) {
+    setUsername(e.target.value);
+  }
 
-    function handlePasswordChange(e) {
-      setPassword(e.target.value);
-    }
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
 
-  function handleLogin(event, username, password){
-    event.preventDefault();
-    
+  function handleLogin(e) {
+    e.preventDefault();
+
     //adresa backend metode
     const backendLoginServiceMethodAddress = `${baseUrl}/api/login`;
     //zahtjev request -> http zahtjev -> zaglavlje + tijelo (header + body)
     const httpRequest = {
-        method: 'POST', 
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({username, password})
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
     };
 
     fetch(backendLoginServiceMethodAddress, httpRequest)
-    .then(response =>{
-        if(response.ok){
-            alert('Logovan si u aplikaciju');
-        }else{
-            alert('Nisi logovan')
+      .then(response => {
+        if (response.ok) {
+          alert('Logovan si u aplikaciju');
+        } else {
+          alert('Nisi logovan')
         }
-    }).catch(error =>{
+      }).catch(error => {
         alert(`${error}`);
-    });
-}
+      });
+  }
 
-    return (
-        <section className="vh-100">
+  return (
+    <section className="vh-100">
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
@@ -77,32 +77,30 @@ function Login () {
                 <p className="text-center fw-bold mx-3 mb-0">Or</p>
               </div>
 
-              
+
               <div className="form-outline mb-4">
                 <label className="form-label" htmlFor="usernameInput">Username</label>
                 <input
-                  handleUsernameChange={handleUsernameChange}
+                  onChange={handleUsernameChange}
                   type="text"
-                  id="usernameInput"
                   className="form-control form-control-lg"
                   placeholder="Enter a valid username"
                 />
               </div>
 
-              
+
               <div className="form-outline mb-3">
                 <label className="form-label" htmlFor="passwordInput">Password</label>
                 <input
-                  handlePasswordChange={handlePasswordChange}
+                  onChange={handlePasswordChange}
                   type="password"
-                  id="passwordInput"
                   className="form-control form-control-lg"
                   placeholder="Enter password"
                 />
               </div>
 
               <div className="d-flex justify-content-between align-items-center">
-                
+
                 <div className="form-check mb-0">
                   <input
                     className="form-check-input me-2"
@@ -122,7 +120,7 @@ function Login () {
                   value="Login"
                   type="submit"
                   className="btn btn-primary btn-lg"
-                  //style={{paddingLeft: 2.5 + 'rem'}, {paddingRight: 2.5 + 'rem'}}
+                //style={{paddingLeft: 2.5 + 'rem'}, {paddingRight: 2.5 + 'rem'}}
                 />
                 <p className="small fw-bold mt-2 pt-1 mb-0">
                   Don't have an account?
@@ -133,9 +131,8 @@ function Login () {
           </div>
         </div>
       </div>
-      <Footter/>
+      <Footter />
     </section>
-    )
+  )
 }
 
-export default Login;
